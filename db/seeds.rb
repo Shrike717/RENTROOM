db1c752fc515c442371303d4ea7d7ca0eed9bc44
Daniel Bauer <dan717@gmx.de>
2022-08-28 17:40:12 +0200

    Seeded user but could not change avatar images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+User.destroy_all
+
+user1 = User.new(email: "user1@gmx.de", password: "123456") # We need different users for testing
+user1.photo.attach(io: File.open("app/assets/images/user1_avatar.png"), filename: "user1_avatar.png", content_type: "image/png")
+
+user1.save!
+
+user2 = User.new(email: "user2@gmx.de", password: "123456") # We need different users for testing
+user2.photo.attach(io: File.open("app/assets/images/user2_avatar.png"), filename: "user2_avatar.png", content_type: "image/png")
+user2.save!
+
+user3 = User.new(email: "user3@gmx.de", password: "123456") # We need different users for testing
+user3.photo.attach(io: File.open("app/assets/images/user3_avatar.png"), filename: "user3_avatar.png", content_type: "image/png")
+user3.save!
